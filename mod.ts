93a9a50f2c42fb3cdaff3323c2482f2090ac0fb0
WhyK <windchimeyk@gmail.com>
2020-12-20 01:55:00 +0900

    style: deno fmt

--- mod.ts
+++ mod.ts
@@ -1,19 +1,19 @@
 import { exec, isExistFileSync, writeFile, readFile, yellow, red } from "./utils/deps.ts"
 
-const [ cmd ] = Deno.args
-const cwdSettingFile = `${Deno.cwd()}/deno-scripts.json`
-const version = 'v0.0.0'
+const [cmd] = Deno.args;
+const cwdSettingFile = `${Deno.cwd()}/deno-scripts.json`;
+const version = "v0.0.0";
 
-if (cmd === 'init' && !isExistFileSync(cwdSettingFile)) {
+if (cmd === "init" && !isExistFileSync(cwdSettingFile)) {
   const sampleData = {
-    "version:deno": "deno -V"
-  }
-  await writeFile(JSON.stringify(sampleData), cwdSettingFile)
-} else if (cmd === 'init' && isExistFileSync(cwdSettingFile)) {
-  console.warn(`${yellow('WARN')} exist config file.`);
-} else if (['-v', 'version'].includes(cmd)) {
+    "version:deno": "deno -V",
+  };
+  await writeFile(JSON.stringify(sampleData), cwdSettingFile);
+} else if (cmd === "init" && isExistFileSync(cwdSettingFile)) {
+  console.warn(`${yellow("WARN")} exist config file.`);
+} else if (["-v", "version"].includes(cmd)) {
   console.log(version);
-} else if (['-h', 'help'].includes(cmd)) {
+} else if (["-h", "help"].includes(cmd)) {
   console.log(`
   dsm ${version}
 
@@ -24,12 +24,14 @@ if (cmd === 'init' && !isExistFileSync(cwdSettingFile)) {
   -v, version : Show version
   -h, help    : Show help
   Other       : Activate the command that matches the text you entered.
-  `)
+  `);
 } else {
   try {
-    const scriptData = await readFile(cwdSettingFile)
-    await exec(JSON.parse(scriptData)[cmd])
+    const scriptData = await readFile(cwdSettingFile);
+    await exec(JSON.parse(scriptData)[cmd]);
   } catch (err) {
-    console.error(`${red('ERR!!')} There are no commands in the configuration file.`)
+    console.error(
+      `${red("ERR!!")} There are no commands in the configuration file.`
+    );
   }
 }
